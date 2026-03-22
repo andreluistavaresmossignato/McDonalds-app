b3b328b470edf430bf92884764a7c1efb7f82695
André Luís Tavares Mossignato <andreluis.mossignato@gmail.com>
2026-03-21 21:11:33 -0300

    Imagens e banco de dados atualizado

--- query_database.sql
+++ query_database.sql
@@ -2,7 +2,14 @@ create database mcdonalds;
 
 use mcdonalds;
 
-CREATE TABLE ingredientes (
+create table pedidos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_cliente VARCHAR(100),
+    total DECIMAL(10,2),
+    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table ingredientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     categoria ENUM('pao', 'recheio', 'complemento'),
@@ -12,17 +19,28 @@ CREATE TABLE ingredientes (
 
 INSERT INTO ingredientes (nome, categoria, preco, imagem) VALUES
 
+-- PÃES 🍞
+('Pão Brioche', 2.50, 'pao', '../assets/paes/brioche.png'),
+('Pão Integral', 3.00, 'pao', '../assets/paes/integral.jpg');
+
 -- RECHEIOS 🍖
-('Hambúrguer 2x Carne', 'recheio', 7.50, 'https://via.placeholder.com/150'),
-('Bacon', 'recheio', 3.00, 'https://via.placeholder.com/150'),
-('Chicken Crispy', 'recheio', 6.00, 'https://via.placeholder.com/150'),
-('Peixe Empanado', 'recheio', 5.50, 'https://via.placeholder.com/150'),
+('Carne', 'recheio', 5.00, '../assets/recheios/carne.jpg'),
+('Bacon', 'recheio', 3.00, '../assets/recheios/bacon.png'),
+('Chicken Crispy', 'recheio', 6.00, '../assets/recheios/chicken-crispy.jpg'),
+('Peixe Empanado', 'recheio', 5.50, '../assets/recheios/peixe.jpg'),
 
 -- COMPLEMENTOS 🥬
-('Tomate', 'complemento', 1.50, 'https://via.placeholder.com/150'),
-('Cebola Roxa', 'complemento', 1.20, 'https://via.placeholder.com/150'),
-('Picles', 'complemento', 1.00, 'https://via.placeholder.com/150'),
-('Molho Especial', 'complemento', 2.00, 'https://via.placeholder.com/150'),
-('Maionese', 'complemento', 1.50, 'https://via.placeholder.com/150'),
-('Ketchup', 'complemento', 1.00, 'https://via.placeholder.com/150'),
-('Mostarda', 'complemento', 1.00, 'https://via.placeholder.com/150');
+('Tomate', 'complemento', 1.50, '../assets/complementos/tomate.avif'),
+('Cebola Roxa', 'complemento', 1.20, '../assets/complementos/cebola-roxa.jpg'),
+('Picles', 'complemento', 1.00, '../assets/complementos/picles.jpg'),
+('Molho Especial', 'complemento', 2.00, '../assets/complementos/molho-especial.jpg'),
+('Maionese', 'complemento', 1.50, '../assets/complementos/maionese.jpg'),
+('Ketchup', 'complemento', 1.00, '../assets/complementos/ketchup.avif'),
+('Mostarda', 'complemento', 1.00, '../assets/complementos/mostarda.jpeg'),
+
+-- BEBIDAS 🍹
+('Coca-Cola', 'bebida', 5.00, '../assets/bebidas/coca-cola.avif'),
+('Coca Zero', 'bebida', 5.00, '../assets/bebidas/coca-zero.avif'),
+('Guaraná', 'bebida', 4.50, '../assets/bebidas/guarana.jpg'),
+('Fanta Laranja', 'bebida', 4.50, '../assets/bebidas/fanta.jpg'),
+('Água', 'bebida', 3.00, '../assets/bebidas/água.png');
